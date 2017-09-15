ef4d1cc88348a3fc24e262e0dd5c26f14f6ba57f
John Pfingst <jpfingst@outlook.com>
2017-09-15 13:04:25 -0400

    Done.

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,6 +11,21 @@ class Rating extends React.Component {
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+    const ratingDiff = nextProps.rating - this.props.rating;
+
+    if (ratingDiff < 0) {
+      this.setState({decreasing: true});
+      this.setState({increasing: false});
+    } else if (ratingDiff > 0) {
+      this.setState({decreasing: false});
+      this.setState({increasing: true});
+    } else {
+      this.setState({decreasing: false});
+      this.setState({increasing: false});
+    }
+  }
+
   render() {
     let trend = 'stable';
     if (this.state.increasing) {
